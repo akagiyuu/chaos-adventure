b237b38791e1f1d0d605f040861ad5d7ea299c38
akagiyuu <khanghuynh0245@gmail.com>
2026-01-16 01:35:51 +0700

    feat(UI): better pause menu

--- Assets/ScriptableObjects/Scene/SceneManagerSO.cs
+++ Assets/ScriptableObjects/Scene/SceneManagerSO.cs
@@ -8,13 +8,9 @@ public class SceneManagerSO : ScriptableObject
 {
     [SerializeField] private GameObject overlayPrefab;
     [SerializeField] private string startScene = "Start";
-    [SerializeField] private string loseScene = "Lose";
-    [SerializeField] private string winScene = "Win";
     [SerializeField] private string levelPrefix = "Level";
 
     public IEnumerator LoadStart() => LoadScene(startScene);
-    public IEnumerator LoadLose() => LoadScene(loseScene);
-    public IEnumerator LoadWin() => LoadScene(winScene);
     public IEnumerator LoadLevel(int level) => LoadScene($"{levelPrefix} {level}");
 
     private IEnumerator LoadScene(string sceneName)
