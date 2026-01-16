d2d22c1847904b870df6e782bf457e30f3494627
akagiyuu <khanghuynh0245@gmail.com>
2026-01-16 10:24:40 +0700

    feat(util): add count entity in layer mask

--- Assets/Scripts/Core/Util.cs
+++ Assets/Scripts/Core/Util.cs
@@ -13,10 +13,20 @@ public static class Util
     public static IEnumerator Periodic(Action callback, float duration)
     {
         var wait = new WaitForSeconds(duration);
-        while(true)
+        while (true)
         {
             callback();
             yield return wait;
         }
+    }
+
+    public static int CountInLayerMask(LayerMask mask)
+    {
+        int count = 0;
+        foreach (var t in UnityEngine.Object.FindObjectsByType<Transform>(FindObjectsSortMode.None))
+        {
+            if (((1 << t.gameObject.layer) & mask.value) != 0) count++;
+        }
+        return count;
     }
 }
